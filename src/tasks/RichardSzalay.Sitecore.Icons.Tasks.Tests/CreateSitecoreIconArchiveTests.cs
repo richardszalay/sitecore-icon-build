314b1127bb58a4d3a48d053306209580a118aeab
Richard Szalay <richard@richardszalay.com>
2017-12-16 06:44:28 +1100

    Move zip task into tasks assembly

---
+++ src/tasks/RichardSzalay.Sitecore.Icons.Tasks.Tests/CreateSitecoreIconArchiveTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml;
+using Xunit;
+
+namespace RichardSzalay.Sitecore.Icons.Tasks.Tests
+{
+    public class CreateSitecoreIconArchiveTests
+    {
+        [Fact]
+        public void NoEntries_ReturnsFalse()
+        {
+            TestArchiveTask(false, new ITaskItem[0]);
+        }
+
+        [Fact]
+        public void ValidEntries_CreatesArchive()
+        {
+            TestArchiveTask(
+                true,
+                new[]
+                {
+                    CreateEntryItem(Path.GetTempFileName(), "fileA"),
+                    CreateEntryItem(Path.GetTempFileName(), "fileB")
+                });
+        }
+
+
+        static ITaskItem CreateEntryItem(string filename, string entryPath)
+        {
+            var taskItem = new TaskItem(filename);
+            taskItem.SetMetadata("EntryPath", entryPath);
+
+            return taskItem;
+        }
+
+        static void TestArchiveTask(bool expectedResult, ITaskItem[] entries)
+        {
+            var task = new CreateSitecoreIconArchive();
+
+            var outputFile = Path.GetTempFileName();
+
+            try
+            {
+
+                task.Entries = entries;
+
+                task.Output = outputFile;
+
+                task.BuildEngine = new FakeBuildEngine();
+
+                var result = task.Execute();
+
+                Assert.Equal(expectedResult, result);
+
+                if (expectedResult)
+                {
+                    var expectedOutput = entries.Select(x => x.GetMetadata("EntryPath"));
+                    var actual = GetArchiveEntries(outputFile);
+
+                    Assert.Equal(expectedOutput.OrderBy(x => x), actual.OrderBy(x => x));
+                }
+            }
+            finally
+            {
+                foreach (var entry in entries)
+                {
+                    try
+                    {
+                        File.Delete(entry.GetMetadata("FullPath"));
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                try
+                {
+                    File.Delete(outputFile);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        static string CreateTempFile(string contents)
+        {
+            var filename = Path.GetTempFileName();
+            File.WriteAllText(filename, contents);
+            return filename;
+        }
+
+        static string[] GetArchiveEntries(string filename)
+        {
+            using (var fileStream = new FileStream(filename, FileMode.Open))
+            {
+                using (var archive = new ZipArchive(fileStream, ZipArchiveMode.Read))
+                {
+                    return archive.Entries.Select(e => e.FullName).ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ src/tasks/RichardSzalay.Sitecore.Icons.Tasks/CreateSitecoreIconArchive.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml;
+
+namespace RichardSzalay.Sitecore.Icons.Tasks
+{
+    public class CreateSitecoreIconArchive : Task
+    {
+        [Required]
+        public ITaskItem[] Entries { get; set; }
+        
+        [Required]
+        public string Output { get; set; }
+
+        public override bool Execute()
+        {
+            if (Entries == null || Entries.Length == 0)
+            {
+                Log.LogError("No entries were supplied");
+                return false;
+            }
+
+            try
+            {
+                const int BufferSize = 64 * 1024;
+
+                var buffer = new byte[BufferSize];
+
+                using (var outputFileStream = new FileStream(Output, FileMode.Create))
+                {
+                    using (var archive = new ZipArchive(outputFileStream, ZipArchiveMode.Create))
+                    {
+                        foreach (var inputFileName in Entries)
+                        {
+                            var inputFileFullPath = inputFileName.GetMetadata("FullPath");
+                            var inputFileEntryPath = inputFileName.GetMetadata("EntryPath");
+
+                            var archiveEntry = archive.CreateEntry(inputFileEntryPath);
+
+                            using (var fs = new FileStream(inputFileFullPath, FileMode.Open))
+                            {
+                                using (var zipStream = archiveEntry.Open())
+                                {
+                                    int bytesRead = -1;
+                                    while ((bytesRead = fs.Read(buffer, 0, BufferSize)) > 0)
+                                    {
+                                        zipStream.Write(buffer, 0, bytesRead);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                return false;
+            }
+        }
+    }
+}
